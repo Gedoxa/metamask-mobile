3ef09e836128a6a6e0c90fb35459c9b4a388380f
JSoufer <jonathan.ferreira@consensys.net>
2024-03-01 13:25:35 +0000

    chore: add constants

--- app/util/number/index.js
+++ app/util/number/index.js
@@ -858,3 +858,9 @@ export const formatValueToMatchTokenDecimals = (value, decimal) => {
   }
   return value;
 };
+
+export const MILLISECOND = 1;
+export const SECOND = MILLISECOND * 1000;
+export const MINUTE = SECOND * 60;
+export const HOUR = MINUTE * 60;
+export const DAY = HOUR * 24;
